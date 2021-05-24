a51280af149a06e5839928c5e5d4776ba451756b
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-05-24 20:39:09 +0200

    Creating Dependency Injection

---
+++ app/src/main/java/com/krolikowski/playyourmusic/MusicApplication.kt
@@ -0,0 +1,7 @@
+package com.krolikowski.playyourmusic
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MusicApplication : Application()
---
+++ app/src/main/java/com/krolikowski/playyourmusic/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.krolikowski.playyourmusic.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.krolikowski.playyourmusic.R
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import java.security.AccessControlContext
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_image)
+            .error(R.drawable.ic_image)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+}
