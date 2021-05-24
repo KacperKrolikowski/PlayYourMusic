c58098abd865552c89ee0cb10a2f0548d0557fc6
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-05-24 20:16:36 +0200

    Song model Get from Firebase function

--- app/src/main/java/com/krolikowski/playyourmusic/data/entities/Song.kt
+++ app/src/main/java/com/krolikowski/playyourmusic/data/entities/Song.kt
@@ -1,4 +1,9 @@
 package com.krolikowski.playyourmusic.data.entities
 
-class Song {
-}
+data class Song (
+    val mediaId: String = "",
+    val title: String = "",
+    val subtitle: String = "",
+    val songUrl: String = "",
+    val imageUrl: String = ""
+        )
---
+++ app/src/main/java/com/krolikowski/playyourmusic/others/Constants.kt
@@ -0,0 +1,6 @@
+package com.krolikowski.playyourmusic.others
+
+object Constants {
+
+    const val SONG_COLLECTION = "songs"
+}
