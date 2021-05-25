3b3739a623725eed15a32d007e7c45698044e2ae
Kacper Królikowski <kaacperkrolikowski@gmail.com>
2021-05-25 11:55:56 +0200

    Dependency Injection Finished

--- app/src/main/java/com/krolikowski/playyourmusic/di/AppModule.kt
+++ app/src/main/java/com/krolikowski/playyourmusic/di/AppModule.kt
@@ -6,15 +6,18 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.krolikowski.playyourmusic.R
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import java.security.AccessControlContext
+import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ApplicationComponent::class)
 object AppModule {
 
+    @Singleton
+    @Provides
     fun provideGlideInstance(
         @ApplicationContext context: Context
     ) = Glide.with(context).setDefaultRequestOptions(
